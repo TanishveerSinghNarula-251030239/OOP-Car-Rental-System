8ef73868a0e189c277e4a457db855254ce724462
Tanish <251030239@juitsolan.in>
2026-03-06 13:44:43 +0530

    Added Vehicle class decleration

---
+++ Vehicle.h
@@ -0,0 +1,19 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <string>
+
+class Vehicle
+{
+protected:
+    int vehicleId;
+    std::string model;
+    double rentPerDay;
+    bool available;
+
+public:
+    Vehicle();
+    void displayVehicle();
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -6,13 +5,4 @@ int main()
 {
     cout << "Car Rental System Project Started" << endl;
     return 0;
-=======
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    cout << "Car Rental System Project Started" << endl;
-    return 0;
->>>>>>> 02973c99d4490dcd8bb92cbe9ac76736a1a4d1b6
 }
